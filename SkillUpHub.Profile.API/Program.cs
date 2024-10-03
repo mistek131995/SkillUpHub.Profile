6e9c76a206258fc2de5f8d261f8d0f7f1eed9f8b
Юрий Кузнецов <omega131995@outlook.com>
2024-10-03 23:16:20 +0800

    Настройка аутентификации

--- SkillUpHub.Profile.API/Program.cs
+++ SkillUpHub.Profile.API/Program.cs
@@ -1,7 +1,43 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SkillUpHub.Profile.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "SkillHub.Auth",
+        ValidAudience = "SkillHub.Services",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("SecretKey").Value)) // Ваш секретный ключ
+    };
+
+    // Важно: поддержка gRPC Web
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+            var accessToken = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                context.Token = accessToken;
+            }
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddAuthorization();
 builder.Services.AddGrpc();
 
 builder.Services.AddCors(x => x.AddPolicy("CORS", builder =>
@@ -14,6 +50,10 @@ builder.Services.AddCors(x => x.AddPolicy("CORS", builder =>
 }));
 
 var app = builder.Build();
+
+// Включаем аутентификацию и авторизацию
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseGrpcWeb();
 app.UseCors();
--- SkillUpHub.Profile.API/Services/ProfileService.cs
+++ SkillUpHub.Profile.API/Services/ProfileService.cs
@@ -1,10 +1,12 @@
 using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
 using SkillUpHub.Profile.API;
 
 namespace SkillUpHub.Profile.API.Services;
 
 public class ProfileService : Profile.ProfileService.ProfileServiceBase
 {
+    [Authorize]
     public async override Task<GetProfileResponse> GetProfile(GetProfileRequest request, ServerCallContext context)
     {
         return new GetProfileResponse()
